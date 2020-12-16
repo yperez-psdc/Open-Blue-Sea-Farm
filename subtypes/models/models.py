3459db8b3dbd76bc4d117a8e9c41a9430b5d92c7
edominguez-psdc <edominguez@psdc.com.pa>
2020-12-16 15:21:30 -0500

    test

--- subtypes/models/models.py
+++ subtypes/models/models.py
@@ -15,7 +15,7 @@ class Subtypes(models.Model):
 	_name = 'helpdesk.ticket'
 	_inherit = 'helpdesk.ticket'
 
-	sale_order_products = fields.Many2many('sale.order.line',  relation='sale_order' )
+	sale_order_products = fields.Many2many('sale.order.line',  relation='sale_order', domain="[('order_id', '=', sale_order_id)]")
 	# column1='product_id'
 	# , domain="[('order_id', '=', 'sale_order_id')]"
 
