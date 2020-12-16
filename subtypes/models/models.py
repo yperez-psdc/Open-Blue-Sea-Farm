1b2328e783d50e4a6338fb5621956cb9c79415ed
edominguez-psdc <edominguez@psdc.com.pa>
2020-12-16 11:47:44 -0500

    test

--- subtypes/models/models.py
+++ subtypes/models/models.py
@@ -15,7 +15,7 @@ class Subtypes(models.Model):
 	_name = 'helpdesk.ticket'
 	_inherit = 'helpdesk.ticket'
 
-	sale_order_products = fields.One2many('sale.order.line' )
+	sale_order_products = fields.Many2many('sale.order.line' )
 	# domain="[('order_id','=','sale_order_id')]"
 
 	# @api.onchange('sale_order_id')
